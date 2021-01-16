741f5614ce4d8ff2fab17ef98c0298243e64bd08
ziad17 <ziadmohamd456@gmail.com>
2021-01-16 14:07:30 +0000

    first

---
+++ CONSTRAINTS.sql
@@ -0,0 +1,63 @@
+/*Person Table*/
+ALTER TABLE Person
+ADD FOREIGN KEY (contact_email) REFERENCES PersonContacts(email);
+ALTER TABLE Person
+ADD FOREIGN KEY (institution_id) REFERENCES Institution(ID);
+ALTER TABLE Person
+ADD FOREIGN KEY (city_shortcut) REFERENCES City(shortcut);
+
+
+/*Professor Table*/
+ALTER TABLE Professor
+ADD FOREIGN KEY (person_id) REFERENCES Person(ID);
+
+/*Admin Table*/
+ALTER TABLE [Admin]
+ADD FOREIGN KEY (person_id) REFERENCES Person(ID);
+
+
+/*Institution Table*/
+ALTER TABLE Institution
+ADD FOREIGN KEY (institution_contacts_email) REFERENCES InstitutionContacts(website_url);
+
+/*Faculty Table*/
+ALTER TABLE Faculty
+ADD FOREIGN KEY (institution_id) REFERENCES Institution(ID);
+
+
+/*Folder Table*/
+ALTER TABLE Folder
+ADD FOREIGN KEY (parent_folder_id) REFERENCES Folder(ID);
+ALTER TABLE Folder
+ADD FOREIGN KEY (author_id) REFERENCES Person(ID);
+ALTER TABLE Folder
+ADD FOREIGN KEY (current_folder_version) REFERENCES FolderVersion(ID);
+
+/*FolderVersion Table*/
+ALTER TABLE FolderVersion
+ADD FOREIGN KEY (folder_id) REFERENCES Folder(ID);
+ALTER TABLE FolderVersion
+ADD FOREIGN KEY (author_id) REFERENCES Person(ID);
+
+
+/*File Table*/
+ALTER TABLE [File]
+ADD FOREIGN KEY (parent_folder_id) REFERENCES Folder(ID);
+ALTER TABLE [File]
+ADD FOREIGN KEY (author_id) REFERENCES Person(ID);
+ALTER TABLE [File]
+ADD FOREIGN KEY (current_file_version) REFERENCES FileVersions(ID);
+
+
+/*FileVersion Table*/
+ALTER TABLE FileVersion
+ADD FOREIGN KEY (file_id) REFERENCES [File](ID);
+ALTER TABLE FileVersion
+ADD FOREIGN KEY (author_id) REFERENCES Person(ID);
+ALTER TABLE FileVersion
+ADD FOREIGN KEY (file_type_extension) REFERENCES FileType(extension);
+ALTER TABLE FileVersion
+ADD FOREIGN KEY (file_content_id) REFERENCES FileContent(ID);
+
+
+
---
+++ CREATION.sql
@@ -0,0 +1,99 @@
+CREATE TABLE Person (
+  ID INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+  first_name varchar(17) NOT NULL,
+  middle_name varchar(30),
+  last_name varchar(17) NOT NULL,
+  contact_email varchar(100) UNIQUE,
+  birth_date DATE NOT NULL,
+  institution_id INT NOT NULL UNIQUE,
+  city_shortcut VARCHAR(5) NOT NULL UNIQUE);
+
+CREATE TABLE City(
+  shortcut VARCHAR(5) PRIMARY KEY NOT NULL,
+  city_name VARCHAR(30) NOT NULL UNIQUE,
+  postal_code INT NOT NULL UNIQUE);
+
+CREATE TABLE PersonContacts(
+email VARCHAR(100) PRIMARY KEY NOT NULL,
+phone_number VARCHAR(11) NOT NULL UNIQUE);
+
+CREATE TABLE Professor(
+  person_id INT NOT NULL UNIQUE,
+  acadmic_number VARCHAR(16) NOT NULL UNIQUE,
+  phd_certificate VARCHAR(100));
+
+CREATE TABLE [Admin](
+ person_id INT NOT NULL UNIQUE);
+
+CREATE TABLE Institution(
+  ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+  institution_name VARCHAR(50) NOT NULL UNIQUE,
+  institution_contacts_website VARCHAR NOT NULL UNIQUE,
+  inside_campus BOOLEAN NOT NULL,
+  active BOOLEAN NOT NULL  );
+
+CREATE TABLE InstitutionContacts(
+  website_url VARCHAR NOT NULL PRIMARY KEY,
+  primary_phone VARCHAR,
+  secondary_phone VARCHAR,
+  fax VARCHAR,
+  email VARCHAR);
+
+CREATE TABLE Faculty(institution_id INT NOT NULL UNIQUE);
+CREATE TABLE DataCenter(institution_id INT NOT NULL UNIQUE);
+
+
+CREATE TABLE Folder(
+ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+parent_folder_id INT NOT NULL,
+date_created DATETIME NOT NULL DEFAULT GETDATE,
+author_id INT NOT NULL,
+current_folder_version INT NOT NULL UNIQUE
+
+);
+
+CREATE TABLE FolderVersion(
+ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+version_name VARCHAR(200) NOT NULL,
+version_notes VARCHAR(1000),
+version_number FLOAT,
+folder_id INT NOT NULL,
+date_created DATETIME NOT NULL DEFAULT GETDATE,
+author_id INT NOT NULL
+);
+
+CREATE TABLE [File](
+ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+parent_folder_id INT NOT NULL,
+date_created DATETIME NOT NULL DEFAULT GETDATE,
+author_id INT NOT NULL,
+current_file_version INT NOT NULL UNIQUE
+);
+
+CREATE TABLE FileType(
+type_name VARCHAR NOT NULL UNIQUE,
+extension VARCHAR NOT NULL UNIQUE
+);
+
+
+CREATE TABLE FileVersion(
+ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+file_id INT NOT NULL,
+version_name VARCHAR(200) NOT NULL,
+version_notes VARCHAR(1000),
+version_number FLOAT,
+date_created DATETIME NOT NULL DEFAULT GETDATE,
+author_id INT NOT NULL,
+file_type_extension VARCHAR NOT NULL,
+file_content_id INT NOT NULL UNIQUE
+);
+
+
+CREATE TABLE FileContent(
+ID INT IDENTITY(1,1) PRIMARY KEY NOT NULL,
+href VARCHAR NOT NULL
+);
+
+
+
+
